e29f3556d56dd5408576b1f06c2a85a8e220c075
Chebawca21 <quba2000@o2.pl>
2023-10-25 15:23:32 +0200

    add train_jade function to test_de

--- test_de.py
+++ test_de.py
@@ -2,18 +2,26 @@ import time
 import numpy as np
 from joblib import Parallel, delayed
 from de import DifferentialEvolution
+from jade import JADE
 
 MAX_FES_10 = 200000
 MAX_FES_20 = 1000000
-N_RUNS = 30
+N_RUNS = 5
 
 D = 10
 FUNC_NUM = 3
 POPULATION_SIZE = 50
+
+# Differential Evolution
 F = 0.8
 CR = 0.8
 MUTATION_TYPE = 'current-to-best'
 CROSSOVER_TYPE = 'bin'
+
+# JADE
+ARCHIVE_SIZE = POPULATION_SIZE
+P = 0.1
+C = 0.1
 
 N_JOBS = -1
 
@@ -25,6 +33,14 @@ def train_de(max_fes):
             break
         de.step()
     return de.best_score
+
+def train_jade(max_fes):
+    jade = JADE(D, FUNC_NUM, POPULATION_SIZE, ARCHIVE_SIZE, P, C)
+    for _ in range(int(max_fes / (POPULATION_SIZE * 2))):
+        if jade.func_evals + (2 * POPULATION_SIZE) > max_fes:
+            break
+        jade.step()
+    return jade.best_score
 
 
 if __name__ == '__main__':
